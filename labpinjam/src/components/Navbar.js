edbd0e0a19be4b0e67dcc1e4fb8bd46de44cf06b
bagussatari <bagussatari@yahoo.com>
2025-08-03 16:41:40 +0700

    Add files via upload

---
+++ labpinjam/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav>
+    <h1>LabPinjam</h1>
+  </nav>
+);
+
+export default Navbar;
---
+++ labpinjam/src/pages/BorrowForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BorrowForm = () => (
+  <div>
+    <h2>Formulir Peminjaman</h2>
+  </div>
+);
+
+export default BorrowForm;
